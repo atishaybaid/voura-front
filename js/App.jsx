f3681be48a35d246ae4ecb3f780a00ddcef1ecd1
atishaybaid <atishaybaid@gmail.com>
2017-10-28 14:11:50 +0530

    webpack devser,react-router added

--- js/App.jsx
+++ js/App.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import {BrowserRouter,Route,Switch} from 'react-router-dom';
+import Landing from './Landing.jsx';
 
-const App = ()=>(<h1>React works</h1>);
+const App = ()=>(
+    <BrowserRouter>
+        <div className="app">
+            <Route  exact path="/" component={Landing} />
+        </div>
+        
+    </BrowserRouter>
+    );
 
 
 ReactDom.render(<App />,document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,9 @@ module.exports = {
         filename:'bundle.js',
         publicPath:'/public/'
     },
+    devServer: {
+    port: 8080
+  },
     devtool:'cheap-eval-source-map',
     resolve:{
         extensions:['.js','.jsx','.json']
