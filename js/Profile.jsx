d444a902d3440ee9af3796105bba80bfbe9b2655
atishaybaid <atishaybaid@gmail.com>
2017-11-17 00:12:37 +0530

    styling profile page done

--- js/Profile.jsx
+++ js/Profile.jsx
@@ -4,6 +4,7 @@ import Divider from 'material-ui/Divider';
 import Chip from 'material-ui/Chip';
 import VrHeader from './VrHeader.jsx'
 import Subheader from 'material-ui/Subheader';
+import '../less/Profile.less';
 
 
 class Profile extends Component{
@@ -12,33 +13,37 @@ class Profile extends Component{
     }
     renderChips(){
      let dummyTags = ['React.js','Redux','Node.js','Html','Css','Less']
-        return  dummyTags.map((selectedTag,index)=>(<Chip key="123">{selectedTag}</Chip>)
+        return  dummyTags.map((selectedTag,index)=>(<Chip style={{'margin':'5px'}}key={index}>{selectedTag}</Chip>)
                         
                     )  
     }
     render(){
         return(
             <div className="profile-page">
-            <VrHeader/>
-                <Card>
+              <div className="profile-card">
+                <VrHeader/>
+                <Card style={{'backgroundColor':'aliceblue','padding':'10px'}}>
                 <div className="profile-card-header">
-                    <img src="images/profile.jpg" alt=""  width="150px" height="150px"/>
+                    <img src="images/profile.jpg" className="profile-image" alt=""  width="150px" height="150px" />
                     <div className="user-info">
-                        <div className="user-name">Atishay Baid</div>
-                        <div className="user-tagline">React.js Developer</div>
+                        <div className="user-name base-pitch">Atishay Baid</div>
+                        <div className="user-tagline sub-pitch">React.js Developer</div>
+                         {/*<Divider style={{margin:'auto',width:'50%'}}/>*/}
+                         <hr className="sep"/>
                         <div className="user-desc">I am a Software Engineer,love to develop applications for web ,because web is open. 
                         I programme in web front end technologies with expertise in JavaScript,HTML,CSS and Frameworks such as React.js,Angular.js .I love to contribute to open source world.</div>
                     </div>
                 </div>
                     <Divider/>
                     <div className="user-education">
-                      <Subheader>Education</Subheader>
-                      <h3>Bachelor of Technology (B.Tech.)</h3>
-                      <h3>JIIT</h3>
+                      <Subheader style={{fontWeight:'600',fontSize:'1em',color:'rgba(0,0,0,.85)'}}>Education</Subheader>
+                      
+                      <div className="edu-int base-txt">JIIT</div>
+                      <div className="edu-dgr base-txt">Bachelor of Technology (B.Tech.)</div>
                     </div>
                     <Divider/>
                     <div className="user-tags">
-                      <Subheader>Skills</Subheader>
+                      <Subheader style={{fontWeight:'600',fontSize:'1em',color:'rgba(0,0,0,.85)'}}>Skills</Subheader>
                       <div className="selected-tags">
                              {this.renderChips()}
                         </div>
@@ -48,6 +53,7 @@ class Profile extends Component{
                 
         
                 </Card>
+              </div>
 
             </div>
             )
